a62d52f8f56779316c4ba1250a5fbb10dd43dff7
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-02-02 08:47:25 -0300

    h

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './componentes/Login';
 import Home from './componentes/Home';
-
+import Hola from "./componentes/Hola"
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Hola/>
       </Routes>
     </Router>
   );
